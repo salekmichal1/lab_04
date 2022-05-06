14207bf6cf942cd3c1444ac7c15b0931c00ef0cc
Michał Sałek <michal.salek@microsoft.wsei.edu.pl>
2022-05-06 23:27:58 +0200

    Dodaj pliki projektów.

---
+++ lab_04/ComposedKey.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_04
+{
+    class ComposedKey
+    {
+        public string KeyA { get; }
+        public string KeyB { get; }
+
+        public ComposedKey(string keyA, string keyB)
+        {
+            this.KeyA = keyA;
+            this.KeyB = keyB;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.KeyA, this.KeyB);
+        }
+
+        public override bool Equals(object? other)
+        {
+            return Equals(other as ComposedKey);
+        }
+
+        public bool Equals(ComposedKey? other)
+        {
+            return other != null
+                && String.Equals(this.KeyA, other.KeyA)
+                && String.Equals(this.KeyB, other.KeyB);
+        }
+
+        public override string ToString()
+        {
+            return $"({this.KeyA} {this.KeyB})";
+        }
+    }
+}
---
+++ lab_04/Program.cs
@@ -0,0 +1,78 @@
+﻿using lab_04;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        IDictionary<string, string[]> types = new Dictionary<string, string[]>();
+
+        types.Add("image", new string[] { "png", "webp", "jpg", "gif", "tiff" });
+        types.Add("audio", new string[] { "ogg", "mp3" });
+        types.Add("video", new string[] { "mkv", "mp4", "webm" });
+        types.Add("document", new string[] { "txt", "doc", "docx", "xml", "xlmx" });
+
+
+        DirectoryInfo root = new DirectoryInfo(@"D:\Program Files\Battlefield V");
+        FileInfo[] allFiles = root.GetFiles("*", SearchOption.AllDirectories);
+        FileInfo[] files = root.GetFiles();
+        DirectoryInfo[] directories = root.GetDirectories();
+
+        Console.WriteLine("Nodes: ");
+        Console.WriteLine(String.Format("{0, 12} {1, 7} {2, 12}", "", "[count]", "[total size]"));
+        Console.WriteLine(String.Format("{0, 12} {1, 6} {2, 12}", "Directories:", directories.Length, $"{ GetDirectioriesSize(allFiles, files) }B"));
+        Console.WriteLine(String.Format("{0, 12} {1, 6} {2, 12}", "Files:", files.Length, $"{ GetFilesSize(files) }B \n"));
+
+        Console.WriteLine("By extensions:");
+        Console.WriteLine(String.Format("{0, 14} {1, 7} {2, 12} {3, 10} {4, 10} {5, 10}", "", "[count]", "[total size]", "[avg size]", "[min size]", "[max size]"));
+
+        IDictionary<string, long[]> extensions = new Dictionary<string, long[]>();
+
+
+        foreach (FileInfo file in allFiles)
+        {
+            string name = Convert.ToBoolean(file.Extension.Length) ? file.Extension : "other";
+
+            if (extensions.ContainsKey(name))
+            {
+                extensions[name] = new long[] { 
+                    extensions[name][0] + 1,
+                    extensions[name][1] + file.Length, 
+                    extensions[name][2] < file.Length ? extensions[name][2] : file.Length,
+                    extensions[name][3] > file.Length ? extensions[name][3] : file.Length
+                };
+            }
+            else
+            {
+                extensions[name] = new long[] { 1, file.Length, file.Length, file.Length };
+            }
+        }
+
+        foreach (KeyValuePair<string, long[]> extension in extensions)
+        {
+            Console.WriteLine(String.Format("{0, 14} {1, 7} {2, 12} {3, 10} {4, 10} {5, 10}", extension.Key, extension.Value[0], extension.Value[1], extension.Value[1] / extension.Value[0], extension.Value[2], extension.Value[3]));
+        }
+
+    }
+    public static long GetFilesSize(FileInfo[] files)
+    {
+        long size = 0;
+        
+        foreach (FileInfo file in files)
+        {
+            size += file.Length;
+        }
+
+        return size;
+    }
+    public static long GetDirectioriesSize(FileInfo[] allFiles, FileInfo[] files)
+    {
+        return GetFilesSize(allFiles) - GetFilesSize(files);
+    }
+
+
+}
